28a79053a885d35be5460a88512b9103b3577eba
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-06-10 22:08:12 +0900

    Add onClieck event and onChanged event.

--- front-app/react-todo/src/App.js
+++ front-app/react-todo/src/App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            changeResult: "change result default.",
+            toDos: [
+                {id: "1", text: "ToDo-1"},
+                {id: "2", text: "ToDo-2"},
+                {id: "3", text: "ToDo-3"}
+            ]
+        }
+    }
+
+    deleteToDos(id) {
+        let toDos = this.state.toDos;
+        toDos = toDos.filter(toDo => toDo.id !== id);
+        this.setState({toDos: toDos})
+    }
+
+    onChange(e) {
+        // Need to user setState function.
+        this.setState({changeResult: e.target.value})
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                    <span>Input & OnChange</span><br/>
+                    <input value={this.state.changeResult} onChange={this.onChange.bind(this)}/><br/>
+                    <span>{this.state.changeResult}</span>
+                </p>
+                <p>
+                    <span>Array & OnClick</span>
+                    <ul>
+                        {
+                            this.state.toDos.map(toDo => {
+                                return (
+                                    <li key={toDo.id}>{toDo.text}
+                                        <button onClick={this.deleteToDos.bind(this, toDo.id)}>del</button>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </p>
+            </div>
+        )
+    }
 }
 
 export default App;
