5773ac4be91fbd1b715e0ce6512b40a8e197d478
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-07-10 21:51:50 +0900

    Add Table component.

--- front-app/react-bootstrap-grid-layout/src/App.js
+++ front-app/react-bootstrap-grid-layout/src/App.js
@@ -1,76 +1,101 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button';
-import CardGroup from 'react-bootstrap/CardGroup'
-import CardDeck from 'react-bootstrap/CardDeck'
-import Card from 'react-bootstrap/Card'
-import Jumbotron from 'react-bootstrap/Jumbotron'
+import Table from 'react-bootstrap/Table'
 
-function App() {
-  return (
-    <Jumbotron className="App m-2">
-        Jumbotron
-        <CardDeck>
-            <Card>
-                <Card.Header>
-                    <Nav variant="tabs" defaultActiveKey="/home">
-                        <Nav.Item>
-                            <Nav.Link href="/home">Active</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item>
-                            <Nav.Link eventKey="disabled" disabled>
-                                Disabled
-                            </Nav.Link>
-                        </Nav.Item>
-                    </Nav>
-                </Card.Header>
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        Test001
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        Test002
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-            <Card>
-                <Card.Img variant="top" src="holder.js/100px160" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        Test003
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-        </CardDeck>
-    </Jumbotron>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            users: [
+                {
+                    nameFirst: 'Tanaka',
+                    nameLast: 'Ichiro',
+                    address: 'Tokyo',
+                    age: 20,
+                    department: 'sales'
+                },
+                {
+                    nameFirst: 'Suzuki',
+                    nameLast: 'Jiro',
+                    address: 'Saitama',
+                    age: 20,
+                    department: 'sales'
+                },
+                {
+                    nameFirst: 'Hayashi',
+                    nameLast: 'Saburo',
+                    address: 'Okinawa',
+                    age: 20,
+                    department: 'sales'
+                }
+            ]
+        }
+    }
+
+    render() {
+        return (
+            <div className="m-4 w-50">
+                <Table striped hover bordered>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Age</th>
+                        <th>Department</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.users.map((user, index) => {
+                            return (
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{user.nameFirst}</td>
+                                    <td>{user.nameLast}</td>
+                                    <td>{user.address}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.department}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </Table>
+                {/* テーブルが入りきらないサイズになると、スクロールバーが表示 */}
+                <Table striped hover bordered responsive>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Address</th>
+                        <th>Age</th>
+                        <th>Department</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {
+                        this.state.users.map((user, index) => {
+                            return (
+                                <tr>
+                                    <td>{index + 1}</td>
+                                    <td>{user.nameFirst}</td>
+                                    <td>{user.nameLast}</td>
+                                    <td>{user.address}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.department}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                    </tbody>
+                </Table>
+            </div>
+        )
+    }
 }
 
 export default App;
---
+++ front-app/react-bootstrap-grid-layout/src/App_Card.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button';
+import CardGroup from 'react-bootstrap/CardGroup'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+function App() {
+  return (
+    <Jumbotron className="App m-2">
+        Jumbotron
+        <CardDeck>
+            <Card>
+                <Card.Header>
+                    <Nav variant="tabs" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link href="/home">Active</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="disabled" disabled>
+                                Disabled
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test001
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test002
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test003
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+        </CardDeck>
+    </Jumbotron>
+  );
+}
+
+export default App;
