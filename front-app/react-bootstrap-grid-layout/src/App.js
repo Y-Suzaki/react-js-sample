88fee7fe59e619a7bdcac1b8284809233c36eb65
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-07-10 21:24:49 +0900

    Add Card sample.

--- front-app/react-bootstrap-grid-layout/src/App.js
+++ front-app/react-bootstrap-grid-layout/src/App.js
@@ -8,113 +8,68 @@ import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
-import FormControl from 'react-bootstrap/FormControl'
-import FormCheck from 'react-bootstrap/FormCheck'
+import CardGroup from 'react-bootstrap/CardGroup'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 function App() {
   return (
-    <div className="App">
-        <Container>
-            <Row>
-                <Col xs={6}>
-                    <Navbar bg="dark" variant="dark" defaultActiveKey="link-1">
-                        <Navbar.Brand>
-                            BootStrap
-                        </Navbar.Brand>
-                        <Nav className="mr-auto">
-                            <Nav.Link eventKey="link-1">Link-1</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link-2</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link-3</Nav.Link>
-                        </Nav>
-                    </Navbar>
-                </Col>
-                <Col xs={6}>
-                    <Navbar bg="light" variant="light" defaultActiveKey="link-1">
-                        <Navbar.Brand>
-                            BootStrap
-                        </Navbar.Brand>
-                    </Navbar>
-                </Col>
-            </Row>
-            <Row>
-                <Col xs={4}>
-                    <Navbar bg="light" variant="light" defaultActiveKey="link-1">
-                        <Nav defaultActiveKey="/home" className="flex-column">
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                            <Nav.Link eventKey="link-1">Link</Nav.Link>
-                        </Nav>
-                    </Navbar>
-                </Col>
-                <Col xs={8}>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>User Name</Form.Label>
-                            <Form.Control type="text"/>
-                            <Form.Text className="text-muted">
-                                Input User Name
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
-                            <Form.Text className="text-muted">
-                                Input Email
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
-                            <Form.Text className="text-muted">
-                                Input Password
-                            </Form.Text>
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Check type="checkbox" label="Check me out" />
-                        </Form.Group>
-                        <Form.Group>
-                            <Form.Label>Select Box</Form.Label>
-                            <Form.Control as="select">
-                                <option>001</option>
-                                <option>002</option>
-                                <option>003</option>
-                            </Form.Control>
-                        </Form.Group>
-                        <Button variant="primary">
-                            Submit
-                        </Button>
-                    </Form>
-                    <hr/>
-                    <Form>
-                        {/* Labelを横並びさせる場合、Grid Layoutを使う */}
-                        <Form.Group as={Row}>
-                            <Form.Label column sm="2">
-                                Email
-                            </Form.Label>
-                            <Col sm="10">
-                                <Form.Control plaintext readOnly defaultValue="email@example.com" />
-                            </Col>
-                        </Form.Group>
-                        <Form.Group as={Row}>
-                            <Form.Label column sm="2">
-                                Email
-                            </Form.Label>
-                            <Col sm="10">
-                                <Form.Control type="password" placeholder="Password" />
-                            </Col>
-                        </Form.Group>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
-    </div>
+    <Jumbotron className="App m-2">
+        Jumbotron
+        <CardDeck>
+            <Card>
+                <Card.Header>
+                    <Nav variant="tabs" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link href="/home">Active</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="disabled" disabled>
+                                Disabled
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test001
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test002
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+            <Card>
+                <Card.Img variant="top" src="holder.js/100px160" />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        Test003
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
+        </CardDeck>
+    </Jumbotron>
   );
 }
 
