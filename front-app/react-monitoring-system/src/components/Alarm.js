f56aebd026271112d7a752ec2dee591e08c67f2f
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-07-22 06:19:08 +0900

    test

--- front-app/react-monitoring-system/src/components/Alarm.js
+++ front-app/react-monitoring-system/src/components/Alarm.js
@@ -1,9 +1,73 @@
 import React from 'react';
+import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
 
 class Alarm extends React.Component {
     render() {
         return (
-            <div>Alarm</div>
+            <div className='pt-3'>
+                <Card className='mt-2 mx-4'>
+                    <Card.Body className='pb-1'>
+                        <Form>
+                            <Form.Group as={Row}>
+                                <Form.Label column>
+                                    閾値設定
+                                </Form.Label>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="2">
+                                    摩耗警告
+                                </Form.Label>
+                                <Col sm="8" className='d-inline-block'>
+                                    摩耗率が
+                                    <div className='d-inline-block'>dd</div>
+                                    <Form.Control className='d-inline-block'/>
+                                    %以下になった場合に通知
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="2">
+                                    摩耗エラー
+                                </Form.Label>
+                                <Col sm="8">
+                                    <Form.Control plaintext readOnly defaultValue="email@example.com" />
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="2">
+                                    通信エラー
+                                </Form.Label>
+                                <Col sm="8">
+                                    <Form.Control plaintext readOnly defaultValue="email@example.com" />
+                                </Col>
+                            </Form.Group>
+                            <hr/>
+                            <Form.Group as={Row}>
+                                <Form.Label column>
+                                    通知先設定
+                                </Form.Label>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm="2">
+                                    Email
+                                </Form.Label>
+                                <Col sm="8">
+                                    <Form.Control plaintext readOnly defaultValue="email@example.com" />
+                                </Col>
+                            </Form.Group>
+                        </Form>
+                    </Card.Body>
+                    <Card.Footer>
+                        <Button variant="info" className='d-block w-25'>更新</Button>
+                    </Card.Footer>
+                </Card>
+            </div>
         )
     }
 }
--- front-app/react-monitoring-system/src/components/UserAccount.js
+++ front-app/react-monitoring-system/src/components/UserAccount.js
@@ -3,6 +3,10 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Table from "react-bootstrap/Table";
+import FormCheck from "react-bootstrap/FormCheck";
+import Pagination from "react-bootstrap/Pagination";
 
 class UserAccount extends React.Component {
     constructor(props) {
@@ -53,6 +57,62 @@ class UserAccount extends React.Component {
                                 </Form.Group>
                             </Form.Row>
                         </Form>
+                    </Card.Body>
+                </Card>
+                <Card className='mt-2 mb-4 mx-4'>
+                    <Card.Body>
+                        <Button variant="outline-info" className='ml-3' style={{width: 100}}>
+                            <FontAwesomeIcon icon="trash-alt"/>
+                            <span className='pl-2'>削除</span>
+                        </Button>
+                        <Table responsive hover className='mt-2'>
+                            <thead>
+                            <tr>
+                                <td className='text-black-50'>
+                                    <FormCheck/>
+                                </td>
+                                <th>ユーザー名</th>
+                                <th>E-mail</th>
+                                <th>部署</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.users.map(user => {
+                                        return (
+                                            <tr>
+                                                <td className='text-black-50'>
+                                                    <FormCheck/>
+                                                </td>
+                                                <td className='text-black-50'>
+                                                    {user.firstName + " " + user.lastName}
+                                                </td>
+                                                <td className='text-black-50'>{user.email}</td>
+                                                <td className='text-black-50'>{user.department}</td>
+                                                <td className='text-black-50 text-center icon-link-custom'>
+                                                    <FontAwesomeIcon icon="edit"/>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })
+                                }
+
+                            </tbody>
+                        </Table>
+                        <div className='d-flex justify-content-between'>
+                            <span className='small text-muted'>1 to 10 of 57</span>
+                            <Pagination>
+                                <Pagination.Prev />
+                                <Pagination.Item active>{1}</Pagination.Item>
+                                <Pagination.Item>{2}</Pagination.Item>
+                                <Pagination.Item>{3}</Pagination.Item>
+                                <Pagination.Item>{4}</Pagination.Item>
+                                <Pagination.Item>{5}</Pagination.Item>
+                                <Pagination.Item>{6}</Pagination.Item>
+                                <Pagination.Next />
+                            </Pagination>
+                        </div>
                     </Card.Body>
                 </Card>
             </div>
