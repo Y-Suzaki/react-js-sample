2165c0852d14bf4f677fe9ddd1cd96a8acf21d02
Y-Suzaki <geranium04.24.1981@gmail.com>
2019-08-19 21:38:21 +0900

    test

--- front-app/react-bootstrap-auth-cognito/src/App.js
+++ front-app/react-bootstrap-auth-cognito/src/App.js
@@ -5,18 +5,52 @@ import Button from 'react-bootstrap/Button'
 import {Link} from "react-router-dom";
 import Users from './components/users'
 // import CallBack from './components/callback'
+import { Bootstrap } from 'a-theme-react';
 import Logs from './components/logs'
-import { getIdToken, signOut } from './auth'
+import { getIdToken, signOut, MySignUp } from './auth'
 import './App.css';
+import {
+    ConfirmSignIn,
+    ConfirmSignUp,
+    ForgotPassword,
+    SignIn,
+    SignUp,
+    VerifyContact
+} from "aws-amplify-react";
 
 // 認証必須のルーティング設定
+// const WithAuth = withAuthenticator(
+//     ({ match }) => (
+//         <Switch>
+//             <Route path="/users" exact={true} component={Users}/>
+//             <Route path="/logs" exact={true} component={Logs}/>
+//         </Switch>
+//     ),
+//     false,
+//     [
+//         <SignIn />,
+//         <ConfirmSignIn />,
+//         <VerifyContact />,
+//         <MySignUp override={'SignUp'}/>,
+//         <ConfirmSignUp />,
+//         <ForgotPassword />,
+//     ],
+//     null,
+//     Bootstrap
+// );
+
 const WithAuth = withAuthenticator(
     ({ match }) => (
         <Switch>
             <Route path="/users" exact={true} component={Users}/>
             <Route path="/logs" exact={true} component={Logs}/>
         </Switch>
-    )
+    ),
+    {
+        signUpConfig: {
+            hiddenDefaults: ["phone_number", "email"]
+        }
+    }
 );
 
 class App extends React.Component {
--- front-app/react-bootstrap-auth-cognito/src/auth.js
+++ front-app/react-bootstrap-auth-cognito/src/auth.js
@@ -1,4 +1,71 @@
-import { Auth } from "aws-amplify";
+import { Auth, I18n } from "aws-amplify";
+import React from 'react';
+import { AmplifyTheme } from 'aws-amplify';
+import {
+    SignUp,
+    FormSection,
+    SectionHeader,
+    SectionBody,
+    SectionFooter,
+    InputRow,
+    ButtonRow,
+    Link,
+} from 'aws-amplify-react';
+
+export class MySignUp extends SignUp {
+    signUp() {
+        const { username, password } = this.inputs;
+        const param = {
+            username: username,
+            password: password
+        }
+        Auth.signUp(param)
+            .then(() => this.changeState('confirmSignUp', username))
+            .catch(err => this.error(err));
+    }
+
+    showComponent(theme) {
+        const { hide } = this.props;
+        // if (hide && hide.includes(SignUp)) { return null; }
+
+        return (
+            <FormSection>
+                <SectionHeader>{I18n.get('Sign Up Account')}</SectionHeader>
+                <SectionBody>
+                    <InputRow
+                        autoFocus
+                        placeholder={I18n.get('Username')}
+                        key="username"
+                        name="username"
+                        onChange={this.handleInputChange}
+                    />
+                    <InputRow
+                        placeholder={I18n.get('Password')}
+                        type="password"
+                        key="password"
+                        name="password"
+                        onChange={this.handleInputChange}
+                    />
+                    <ButtonRow onClick={this.signUp}>
+                        {I18n.get('Sign Up')}
+                    </ButtonRow>
+                </SectionBody>
+                <SectionFooter>
+                    <div>
+                        <Link onClick={() => this.changeState('confirmSignUp')}>
+                            {I18n.get('Confirm a Code')}
+                        </Link>
+                    </div>
+                    <div style={Object.assign({textAlign: 'right'}, theme.col6)}>
+                        <Link onClick={() => this.changeState('signIn')}>
+                            {I18n.get('Sign In')}
+                        </Link>
+                    </div>
+                </SectionFooter>
+            </FormSection>
+        )
+    }
+};
 
 export const getIdToken = async () => {
     // Refresh Token周りの制御も下記で自動にやってくれるらしいが、未確認
@@ -13,5 +80,5 @@ export const signOut = async () => {
 export const authConfig = {
     region: "us-west-2",
     userPoolId: "us-west-2_7ahbXqGso",
-    userPoolWebClientId: "",
+    userPoolWebClientId: "175keeno8e83vrtdl2c4gt7hon",
 };
